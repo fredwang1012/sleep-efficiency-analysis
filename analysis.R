cc3fa99a8b97e769ad476f7cb4612cde8096cbf9
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-08-01 12:05:22 -0700

    imported data from csv and a simple lin reg

--- analysis.R
+++ analysis.R
@@ -1,1 +1,9 @@
-print('hello world')
+dataset = read.table('Sleep_Efficiency.csv', TRUE, ',')
+dataset = na.omit(dataset)
+dataset
+
+dataset$Smoking.status = as.factor(dataset$Smoking.status)
+dataset
+
+scuffed = lm(Sleep.efficiency ~ ., data = dataset)
+summary(scuffed)

a5f01b4088a81f6a744bea6cc1570a128bb88d26
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-08-01 19:53:02 -0700

    prelim analysis and visualization

--- analysis.R
+++ analysis.R
@@ -1,9 +1,20 @@
 dataset = read.table('Sleep_Efficiency.csv', TRUE, ',')
 dataset = na.omit(dataset)
-dataset
 
 dataset$Smoking.status = as.factor(dataset$Smoking.status)
+dataset$Gender = as.factor(dataset$Gender)
+dataset$Awakenings = as.factor(dataset$Awakenings)
+dataset$Exercise.frequency = as.factor(dataset$Exercise.frequency)
+
 dataset
 
-scuffed = lm(Sleep.efficiency ~ ., data = dataset)
+scuffed = lm(Sleep.efficiency ~ Age + Caffeine.consumption + 
+               Alcohol.consumption + Smoking.status + Exercise.frequency + Awakenings + 
+               Deep.sleep.percentage, data = dataset)
 summary(scuffed)
+
+resids = scuffed$residuals
+
+resids_plot = plot(dataset$Sleep.efficiency, resids)
+
+qq_plot = qqnorm(resids)

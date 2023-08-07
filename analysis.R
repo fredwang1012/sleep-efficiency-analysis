3fccc0a99fd84b614c0cbaee0289e9b1499437b0
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-08-07 16:56:39 -0700

    converted bedtime into hours after 9pm and wakeup time into hours after 3am

--- analysis.R
+++ analysis.R
@@ -1,3 +1,6 @@
+#Loading Libraries
+library(lubridate)
+
 
 #Data preprocessing
 dataset = read.table('Sleep_Efficiency.csv', TRUE, ',')
@@ -5,19 +8,35 @@ dataset = na.omit(dataset)
 
 dataset$Smoking.status = as.factor(dataset$Smoking.status)
 dataset$Gender = as.factor(dataset$Gender)
+dataset$Bedtime = as.POSIXlt(dataset$Bedtime)
+dataset$Wakeup.time = as.POSIXlt(dataset$Wakeup.time)
 
-dataset
+#Turn bedtime into number of hours after 9:00pm
+hours = as.numeric(hour(dataset$Bedtime))
+hours = as.numeric(hours + minute(dataset$Bedtime)/60)
+ifelse(21-hours <= 0, hours - 21, hours + 3)
+dataset$Bedtime = hours
+dataset$Bedtime
+
+#Turn wake up time into hours since 3:00am
+hours = as.numeric(hour(dataset$Wakeup.time))
+hours = as.numeric(hours + minute(dataset$Wakeup.time)/60)
+hours = hours - 3
+dataset$Wakeup.time = hours
+dataset$Wakeup.time
+
 
 
 #Main model selection
 full_model = lm(Sleep.efficiency ~ Age + Gender + Sleep.duration + REM.sleep.percentage + Deep.sleep.percentage + Light.sleep.percentage + Awakenings + Caffeine.consumption +
-                 Alcohol.consumption + Smoking.status + Exercise.frequency, data = dataset)
+                 Alcohol.consumption + Smoking.status + Exercise.frequency + Bedtime + Wakeup.time, data = dataset)
 summary(full_model)
 
 resids = full_model$residuals
 fitted = full_model$fitted.values
 
-resids_plot = plot(fitted, resids)
+resids_plot = plot(fitted, resids, xlab = 'Fitted Sleep Efficiency', ylab = 'Residuals')
+title('Plot of Residuals vs Fitted Sleep Efficiency for Full Model')
 
 
 
@@ -25,13 +44,15 @@ optimized = lm(Sleep.efficiency ~ Age + REM.sleep.percentage + Deep.sleep.percen
             Alcohol.consumption + Smoking.status + Exercise.frequency, data = dataset)
 summary(optimized)
 
-resids = optimized$residuals
-fitted = optimized$fitted.values
+optimized_resids = optimized$residuals
+optimized_fitted = optimized$fitted.values
 
-resids_plot = plot(fitted, resids)
+resids_plot = plot(optimized_fitted, optimized_resids, xlab = 'Fitted Sleep Efficiency', ylab = 'Residuals')
+title('Plot of Residuals vs Fitted Sleep Efficiency for Optimized Model')
 
-qq_plot = qqnorm(resids)
-qqline(resids)
+
+qq_plot = qqnorm(optimized_resids, ylab = 'Optimized Model Residuals Sample Quantiles')
+qqline(optimized_resids)
 
 
 #Other relevant models

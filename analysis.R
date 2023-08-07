eafb04bcd7ef6541483ed0e931a869d7edaa9c7a
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-08-06 19:26:49 -0700

    Added intermediate steps to how the final optimized model was achieved.

--- analysis.R
+++ analysis.R
@@ -1,3 +1,5 @@
+
+#Data preprocessing
 dataset = read.table('Sleep_Efficiency.csv', TRUE, ',')
 dataset = na.omit(dataset)
 
@@ -6,13 +8,25 @@ dataset$Gender = as.factor(dataset$Gender)
 
 dataset
 
-scuffed = lm(Sleep.efficiency ~ Age + Caffeine.consumption + 
-               Alcohol.consumption + Smoking.status + Exercise.frequency + Awakenings + 
-               Deep.sleep.percentage, data = dataset)
-summary(scuffed)
 
-resids = scuffed$residuals
-fitted = scuffed$fitted.values
+#Main model selection
+full_model = lm(Sleep.efficiency ~ Age + Gender + Sleep.duration + REM.sleep.percentage + Deep.sleep.percentage + Light.sleep.percentage + Awakenings + Caffeine.consumption +
+                 Alcohol.consumption + Smoking.status + Exercise.frequency, data = dataset)
+summary(full_model)
+
+resids = full_model$residuals
+fitted = full_model$fitted.values
+
+resids_plot = plot(fitted, resids)
+
+
+
+optimized = lm(Sleep.efficiency ~ Age + REM.sleep.percentage + Deep.sleep.percentage + Awakenings + Caffeine.consumption +
+            Alcohol.consumption + Smoking.status + Exercise.frequency, data = dataset)
+summary(optimized)
+
+resids = optimized$residuals
+fitted = optimized$fitted.values
 
 resids_plot = plot(fitted, resids)
 
@@ -20,12 +34,18 @@ qq_plot = qqnorm(resids)
 qqline(resids)
 
 
+#Other relevant models
 age_vs_efficiency = lm(Sleep.efficiency ~ Age, data = dataset)
 summary(age_vs_efficiency)
+plot(dataset$Age, dataset$Sleep.efficiency)
+plot(age_vs_efficiency$fitted.values, age_vs_efficiency$residuals)
 
 age_vs_duration = lm(Sleep.duration ~ Age, data = dataset)
 summary(age_vs_duration)
-
+plot(dataset$Age, dataset$Sleep.duration)
+plot(age_vs_duration$fitted.values, age_vs_duration$residuals)
 
 duration_vs_efficiency = lm(Sleep.efficiency ~ Sleep.duration, data = dataset)
 summary(duration_vs_efficiency)
+plot(dataset$Sleep.duration, dataset$Sleep.efficiency)
+plot(duration_vs_efficiency$fitted.values, duration_vs_efficiency$residuals)
